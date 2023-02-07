de0dc880f6c73acdefb699240e7efba34e5cd55f
ink0rr <tomflynnbeltsazar@gmail.com>
2023-02-08 05:40:18 +0700

    Add gdocs blob

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { serve } from "https://deno.land/std@0.176.0/http/server.ts";
 import { cors } from "https://deno.land/x/hono@v2.7.6/middleware.ts";
 import { Hono } from "https://deno.land/x/hono@v2.7.6/mod.ts";
+import { createBlob } from "./gdocs.ts";
 import { createRecipeImage } from "./recipe.ts";
 import { parseUrl } from "./url.ts";
 
 export const app = new Hono();
 
-app.use("/", cors());
+app.use("*", cors());
 
 app.get("/", async (ctx) => {
   const { input, output, customItems } = parseUrl(ctx.req.url);
@@ -16,6 +17,16 @@ app.get("/", async (ctx) => {
   ctx.res.headers.set("Content-Type", "image/png");
 
   return ctx.newResponse(await image.encode(), 200);
+});
+
+app.get("/gdocs", async (ctx) => {
+  const { origin, search } = new URL(ctx.req.url);
+  const blob = createBlob(origin, search);
+
+  ctx.res.headers.set("Cache-Control", "max-age=2592000");
+  ctx.res.headers.set("Content-Type", "text/html");
+
+  return ctx.newResponse(await blob.text(), 200);
 });
 
 serve(app.fetch);
