d405715ae11eb3385b7fda3969d286e8353b771e
ink0rr <tomflynnbeltsazar@gmail.com>
2023-07-14 02:26:20 +0700

    Change target domain

--- src/gdocs.ts
+++ src/gdocs.ts
@@ -4,7 +4,7 @@
 export function createBlob(origin: string, params: string) {
   // The extra space before <img> tag is required for the link to be inserted to the image
   return new Blob([
-    `<meta charset="utf-8"><b style="font-weight:400"><a href="https://recipe.ink0rr.site/${params}" style="text-decoration:none;visibility:hidden"><span style="color: #000000;vertical-align:baseline;white-space:pre;white-space:pre-wrap"><span style="border:none;display:inline-block;overflow:hidden;width:380px;height:168px">  <img src="${origin}/${params}" width="380" height="168" style="margin-left:0;margin-top:0"></span></span></a></b>`,
+    `<meta charset="utf-8"><b style="font-weight:400"><a href="https://recipe.ink0rr.dev/${params}" style="text-decoration:none;visibility:hidden"><span style="color: #000000;vertical-align:baseline;white-space:pre;white-space:pre-wrap"><span style="border:none;display:inline-block;overflow:hidden;width:380px;height:168px">  <img src="${origin}/${params}" width="380" height="168" style="margin-left:0;margin-top:0"></span></span></a></b>`,
   ], {
     type: "text/html",
   });
