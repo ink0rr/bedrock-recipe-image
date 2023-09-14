aae883b2f616bb2c609eb9f020b22e5569e182d8
ink0rr <tomflynnbeltsazar@gmail.com>
2023-09-15 03:04:36 +0700

    Fix gdocs blob

--- src/gdocs.ts
+++ src/gdocs.ts
@@ -2,10 +2,7 @@
  * Create a blob to be used in Google Docs
  */
 export function createBlob(origin: string, params: string) {
-  // The extra space before <img> tag is required for the link to be inserted to the image
   return new Blob([
-    `<meta charset="utf-8"><b style="font-weight:400"><a href="https://recipe.ink0rr.dev/${params}" style="text-decoration:none;visibility:hidden"><span style="color: #000000;vertical-align:baseline;white-space:pre;white-space:pre-wrap"><span style="border:none;display:inline-block;overflow:hidden;width:380px;height:168px">  <img src="${origin}/${params}" width="380" height="168" style="margin-left:0;margin-top:0"></span></span></a></b>`,
-  ], {
-    type: "text/html",
-  });
+    `<meta charset="utf-8"><b><a href="https://recipe.ink0rr.dev/${params}" style="text-decoration:none;"><span><span>&hairsp;<img src="${origin}/${params}"></span></span></a></b>`,
+  ], { type: "text/html" });
 }
